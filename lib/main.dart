ce317f7a20894324ef27a766deeeec18278b616e
scarletjames896 <scarletjames896@gmail.com>
2023-01-19 01:32:48 +0500

    new change

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Neumorphic_Button(),
     );
   }
--- lib/neumorphic.dart
+++ lib/neumorphic.dart
@@ -12,11 +12,30 @@ class _Neumorphic_ButtonState extends State<Neumorphic_Button> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFE4EBF1),
       body: Center(
         child: Container(
-          height: 400,
-          width: 400,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+              color: Color(0xFFE4EBF1),
+              borderRadius: BorderRadius.circular(27),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black54.withOpacity(0.23),
+                    offset: Offset(5, 5),
+                    // spreadRadius: 1,
+                    blurRadius: 10),
+                BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, -5),
+                    // spreadRadius: 1,
+                    blurRadius: 10),
+              ]),
+          child: Icon(
+            Icons.light_mode_outlined,
+            size: 20,
+          ),
         ),
       ),
     );
